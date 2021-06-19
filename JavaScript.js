adfd8370108915f7316ee21b6fd5ab0f3c874edd
AnasOsta <83444950+AnasOsta@users.noreply.github.com>
2021-06-20 01:53:39 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num
+}
+
+function equal() {
+    var exp = document.form.textview.value
+    if (exp) {
+        document.form.textview.value = eval(exp)
+    }
+}
+
+function clean() {
+    document.form.textview.value = ""
+}
